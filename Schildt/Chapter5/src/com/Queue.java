2d8407fc1356cb940f1c24c676683f843e6c1094
Mikhail Permyakov <mikhailpermyakov@yahoo.com>
2017-07-31 15:57:23 +0700

    Chapter 5

---
+++ Schildt/Chapter5/src/com/Queue.java
@@ -0,0 +1,28 @@
+package com;
+
+class Queue {
+    Object objects[];
+    int putloc, getloc;
+
+    Queue(int size) {
+        objects = new Object[size + 1];
+        putloc = getloc = 0;
+    }
+
+    void put(Object obj){
+        if (putloc == objects.length -1) {
+            System.out.println("Очередь заполнена");
+            return;
+        }
+        putloc++;
+        objects[putloc] = obj;
+    }
+    Object get(){
+        if (getloc == putloc){
+            System.out.println("Очередь пуста");
+            return (char)0;
+        }
+        getloc++;
+        return objects[getloc];
+    }
+}
