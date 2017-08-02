acf99328b375bc611dc12d9ce323a32e68b3ba01
Mikhail Permyakov <mikhailpermyakov@yahoo.com>
2017-08-02 18:14:24 +0700

    Chapter 6

--- Schildt/Chapter6/src/com/queue/QDemo.java
+++ Schildt/Chapter6/src/com/queue/QDemo.java
@@ -1,39 +1,92 @@
 package com.queue;
 
 
+class Queue {
+    private char q[];
+    private int putloc, getloc;
+
+    Queue(int size) {
+        q = new char[size + 1];
+        putloc = getloc = 0;
+    }
+
+    Queue (Queue ob){
+        putloc = ob.putloc;
+        getloc = ob.getloc;
+        q = new char[ob.q.length];
+
+        for (int i = getloc + 1; i <= putloc; i++) {
+            q[i] = ob.q[i];
+        }
+    }
+
+    Queue(char a[]){
+        putloc = 0;
+        getloc = 0;
+        q = new char[a.length + 1];
+
+        for (char anA : a) put(anA);
+    }
+    void put(char obj){
+        if (putloc == q.length -1) {
+            System.out.println("\nОчередь заполнена");
+            return;
+        }
+        putloc++;
+        q[putloc] = obj;
+    }
+    char get(){
+        if (getloc == putloc){
+            System.out.println("Очередь пуста");
+            return (char)0;
+        }
+        getloc++;
+        return q[getloc];
+    }
+}
+
 public class QDemo {
 
     public static void main(String[] args) {
-        Queue bigQ = new Queue(100);
-        Queue smallQ = new Queue(4);
+        //create empty queue that stores 10 elements
+        Queue q1 = new Queue(10);
+
+        char name[] = {'T', 'o', 'm'};
+        //create queue based on the given array
+        Queue q2 = new Queue(name);
+
         char ch;
         int i;
 
-        System.out.println("using bigQ to save alphabet");
-        for (i = 0; i < 26; i++) {
-            bigQ.put((char)('A' + i));
+        //put a symbol set in the queue q1
+        for (i = 0; i < 10; i++) {
+            q1.put((char)('A' + i));
         }
 
-        System.out.println("content of bigQ: ");
-        for (i = 0; i < 26; i++) {
-            ch = (char)bigQ.get();
-            if(ch != (char)0) System.out.println(ch);
+        //create a queue based on another one
+        Queue q3 = new Queue(q1);
+
+        //show queues
+        System.out.print("q1 content: ");
+        for (i = 0; i < 10; i++) {
+            ch = q1.get();
+            System.out.print(ch);
         }
-        System.out.println();
+        System.out.println("\n");
 
-        System.out.println("using smallQ to generate errors");
-        for (i = 0; i < 5; i++) {
-            System.out.print("trying to save " +  (char)('Z' - i));
-            smallQ.put((char)('Z' - i));
-            System.out.println();
+        System.out.println("q2 content: ");
+        for (i = 0; i < 3; i++) {
+            ch = q2.get();
+            System.out.println(ch);
+
         }
-        System.out.println();
 
-        System.out.println("smallQ content: ");
-        for (i = 0; i < 5; i++) {
-            ch = (char)smallQ.get();
-            if(ch != (char) 0) System.out.println(ch);
+        System.out.println("\n");
 
+        System.out.println("q3 content: ");
+        for (i = 0; i < 10; i++) {
+            ch = q3.get();
+            System.out.print(ch);
         }
     }
 }
